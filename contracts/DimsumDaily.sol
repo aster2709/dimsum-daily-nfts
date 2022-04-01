7027d6322a628fe62d1e005e542bc83c3c237212
aster2709 <ymehta777@gmail.com>
2022-04-01 18:39:54 +0530

    feat: NFT contract using erc721a

---
+++ contracts/DimsumDaily.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DimsumDaily is ERC721A, Ownable {
+    uint256 public price = 0.08 ether;
+    string internal _uri;
+
+    event Deploy();
+
+    constructor(uint256 _price, string memory uri_)
+        ERC721A("Dimsum Daily", "DIMSUM")
+    {
+        price = _price;
+        _uri = uri_;
+        emit Deploy();
+    }
+
+    function buy(uint256 _quantity) public payable {
+        require(msg.value == _quantity * price, "Invalid price");
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    function setUri(string memory uri_) public onlyOwner {
+        _uri = uri_;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 balance = getBalance();
+        payable(owner()).transfer(balance);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function _startTokenId() internal pure override returns (uint256) {
+        return 1;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _uri;
+    }
+}
