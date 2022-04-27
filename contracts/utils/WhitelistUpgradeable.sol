a5097dd659b1c65d5eeee93ec578979f6e4bf1e4
aster2709 <ymehta777@gmail.com>
2022-04-27 16:18:36 +0530

    fix: invoke ownable constructor

--- contracts/utils/WhitelistUpgradeable.sol
+++ contracts/utils/WhitelistUpgradeable.sol
@@ -12,6 +12,13 @@ contract WhitelistUpgradeable is OwnableUpgradeable {
     event Whitelisted(address indexed account);
     event Unwhitelisted(address indexed account);
 
+    function __Whitelist_init() internal onlyInitializing {
+        __Whitelist_init_unchained();
+        __Ownable_init();
+    }
+
+    function __Whitelist_init_unchained() internal onlyInitializing {}
+
     function addWhitelist(address _account) public onlyOwner {
         require(!whitelist[_account], "WL: account already whitelisted");
         whitelist[_account] = true;
